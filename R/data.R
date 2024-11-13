1fcb6c922e4003cfb5c854d0946876751c155886
ik936 <ik936@mgb033123.lan>
2024-11-13 05:45:05 -0500

    added tessera_warmup as package data

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Sample data for Tessera vignettes
+#'
+#' Subset of MERFISH lung cancer dataset (Chen et al 2024). 
+#'
+#' @format counts and meta data table for 3177 cells. 
+#' \describe{
+#'   \item{counts}{dgCMatrix of gene-by-cell counts}
+#'   \item{meta_data}{data.frame with spatial locations (XY) and cell lineage (type)}
+#' }
+#' @source Chen, J.H., Nieman, L.T., Spurrell, M. et al. Human lung cancer harbors spatially organized stem-immunity hubs associated with response to immunotherapy. Nat Immunol 25, 644–658 (2024). https://doi.org/10.1038/s41590-024-01792-2
+"tessera_warmup"
